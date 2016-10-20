4c7d124d44af19003f91d781f7955b91d76035b5
François Mentec <francois.mentec@hotmail.fr>
2016-10-20 15:04:27 +0200

    Impressive UI

--- src/solidText/Buffer.java
+++ src/solidText/Buffer.java
@@ -1,5 +1,7 @@
 package solidText;
 
+import java.util.ArrayList;
+
 public class Buffer {
 	public static final String CSS_STYLE = "<style>"
 			+ "body{"
@@ -12,6 +14,17 @@ public class Buffer {
 				+ "line-height: 1em;"
 				+ "margin: 0;"
 				+ "overflow: hidden;"
+			+ "}"
+			+ ""
+			+ ".line-number{"
+				+ "display: inline-block;"
+				+ "font-size: 0.8em;"
+				+ "width: 3em;"
+				+ "text-align: right;"
+				+ "border-right: 1px solid gray;"
+				+ "color: gray;"
+				+ "padding-right: 3px;"
+				+ "margin-right: 3px;"
 			+ "}"
 			+ ""
 			+ ".current-line{"
@@ -39,7 +52,7 @@ public class Buffer {
 			+ "}"
 			+ ""
 			+ ".selection{"
-				+ "background-color: blue;"
+				+ "background-color: #3399ff;"
 				+ "color: white;"
 			+ "}"
 			+ "</style>";
@@ -77,6 +90,10 @@ public class Buffer {
 	public String toString(){
 		return text;
 	}
+	
+	public boolean inSelect(int pos){
+		return pos>=Math.min(startSelect, endSelect) && pos<=Math.max(startSelect, endSelect);
+	}
 
 	public void addText(String text) {
 		//System.out.println("Add text ["+text+"]");
@@ -94,40 +111,80 @@ public class Buffer {
 	}
 
 	public String toHtml() {
-		/*String html = text.substring(0, startSelect) + "<span class=\"cursor\"></span>" + text.substring(startSelect);
-		html = "<html>" + CSS_STYLE + "<body>" + html.replace("\n", "<br>").replace("\r", "<br>") + "</body></html>";*/
-		String html = text.substring(0, Math.min(startSelect, endSelect))
-				+ "<span class=\"cursor\"></span><span class=\"selection\">"
-				+ text.substring(Math.min(startSelect, endSelect), Math.max(startSelect, endSelect))
-				+ "</span>"
-				+ text.substring(Math.max(startSelect, endSelect));
-		String str = "";
+		boolean inSelect = false;
+		boolean currentLine = false;
+		int nbLine = 1; 
+		String html = "";
 		String line = "";
-		int start = 0;
-		for(int i=0;i<html.length();i++){
-			if(html.charAt(i)!='\r'){
-				line += html.charAt(i);
-			}else{
-				if(startSelect>=start && startSelect<=i){
-					line = "<div class=\"line current-line\">" + line + "</div>";
-				}else{
-					line = "<div class=\"line\">" + line + "</div>";
+		
+		//Affichage du curseur (Cas particulier)
+		if(endSelect==0){
+			line += "<span class=\"cursor\"></span>";
+			currentLine = true;
+		}
+		
+		for(int i=0;i<text.length();i++){
+			char c = text.charAt(i);
+			
+			//System.out.println("i = "+i+" s = "+startSelect+" e = "+endSelect);
+			
+			//Affichage de la selection
+			if(i==Math.min(startSelect, endSelect)){
+				line += "<span class=\"selection\">";
+				inSelect = true;
+				currentLine = true;
+			}
+			
+			if(i==Math.max(startSelect, endSelect)){
+				line += "</span>";
+				inSelect = false;
+			}
+			
+			
+			if(c=='\r' || c=='\n'){
+				//Gestion du retour à ligne
+				if(inSelect){
+					line += "</span>";
 				}
-				str += line;
-				start = i+1;
+				
+				if(currentLine){
+					html += "<div class=\"line current-line\">";
+				}else{
+					html += "<div class=\"line\">";
+				}
+				html += "<span class=\"line-number\">" + nbLine + "</span>" + line + "</div>";
+				nbLine++;
+				
 				line = "";
+				if(!inSelect){
+					currentLine = false;//Seul endroit où current line peut-être mit à false
+				}
+				
+				if(inSelect){
+					line += "<span class=\"selection\">";
+				}
+			}else{
+				line += c;//Affichage du caractère
+			}
+			
+			//Affichage du curseur
+			if(i+1==endSelect){
+				line += "<span class=\"cursor\"></span>";
+				currentLine = true;
 			}
 		}
-		if(startSelect>=start && startSelect<=html.length()){
-			line = "<div class=\"line current-line\">" + line + "</div>";
-		}else{
-			line = "<div class=\"line\">" + line + "</div>";
-		}
-		str += line;
 		
-		html = str;
+		//Affichage de la dernière ligne (hors du for)
+		if(currentLine){
+			html += "<div class=\"line current-line\">";
+		}else{
+			html += "<div class=\"line\">";
+		}
+		html += "<span class=\"line-number\">" + nbLine + "</span>" + line + "</div>";
+		nbLine++;
+		
+		//Concaténation du contenu avec le reste de la vue
 		html = "<html>" + CSS_STYLE + "<body>" + html + "</body></html>";
-		//html = "<html>" + CSS_STYLE + "<body><div class=\"line\">" + html.replace("\n", "</div><div class=\"line\">").replace("\r", "</div><div class=\"line\">") + "</div></body></html>";
 		return html;
 	}
 }
