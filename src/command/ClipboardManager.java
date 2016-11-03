22f29df24ae6ecb9cceb4e6b8e7a1e62cb2dfc55
Olivier Pelgrin <olivier.pelgrin@etudiant.univ-rennes1.fr>
2016-11-03 15:19:41 +0100

    Add of missing file

---
+++ src/command/ClipboardManager.java
@@ -0,0 +1,24 @@
+package command;
+
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
+
+public class ClipboardManager {
+	
+	private static Clipboard clipboard = Clipboard.getSystemClipboard();
+	
+	public static String getContent(){
+		String r = clipboard.getString();
+		if(r == null) {
+			r = "";
+		}
+		return r;
+	}
+	
+	public static void setContent(String content) {
+		ClipboardContent cc = new ClipboardContent();
+		cc.putString(content);
+		clipboard.setContent(cc);
+	}
+
+}
