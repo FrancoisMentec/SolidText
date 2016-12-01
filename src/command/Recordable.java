bbcef942c2b631dbb962c154270f23bed7690e32
Olivier Pelgrin <olivier.pelgrin@etudiant.univ-rennes1.fr>
2016-12-01 13:35:13 +0100

    comments

--- src/command/Recordable.java
+++ src/command/Recordable.java
@@ -2,6 +2,8 @@ package command;
 
 // A command that implement this interface is recordable as part of a macro
 // Recordable Commands should save theirs data in order to be executed again when we play the macro (Memento)
+// Recordable Commands are Originators and ConcreteMemento in this implementation
+
 
 public interface Recordable {
 	public Command copy();
